a31fb3c6f3dd593c04d8ed03656b4dd94e6875e1
moerwald <andreas.mewald@gmail.com>
2019-01-03 11:44:21 +0100

    Added unit tests

---
+++ TplDataflow.Extension.Tests/TargetBlockWrapperTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using TplDataFlow.Extension.TaskScheduler.Synchronous;
+using TplDataFlow.Extension.TplDataBlockWrapper;
+
+namespace TplDataflow.Extension.Tests
+{
+    [TestFixture]
+    public class TargetBlockWrapperTests
+    {
+        [Test]
+        public void CtorThrowNullException() => Assert.Throws<ArgumentNullException>(() => new TargetBlockWrapper<int>(null));
+
+        [Test]
+        public void PostActsAsProxy()
+        {
+            int receivedValue = 0;
+            var ts = new TaskSchedulerSingleThreaded();
+            var ab = new ActionBlock<int>(i => receivedValue = i, new ExecutionDataflowBlockOptions { TaskScheduler = ts });
+            new TargetBlockWrapper<int>(ab).Post(42);
+
+            ts.ExecuteAllTasksInActualThread();
+            Assert.That(receivedValue, Is.EqualTo(42));
+        }
+
+        [Test]
+        public async Task SendAsyncActsAsProxy()
+        {
+            int receivedValue = 0;
+            var ts = new TaskSchedulerSingleThreaded();
+            var ab = new ActionBlock<int>(i => receivedValue = i, new ExecutionDataflowBlockOptions { TaskScheduler = ts });
+            var task = new TargetBlockWrapper<int>(ab).SendAsync(42);
+
+            ts.ExecuteAllTasksInActualThread();
+            await task;
+            Assert.That(receivedValue, Is.EqualTo(42));
+        }
+    }
+}
